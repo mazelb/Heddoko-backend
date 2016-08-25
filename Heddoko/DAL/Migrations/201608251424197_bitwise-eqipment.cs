72b9a48eff51da7f0da0951ae9562fd1f921e452
knopa <klepa.klepa@gmail.com>
2016-08-25 20:24:50 +0300

    fixed bitwise equipment migrations

--- Heddoko/DAL/Migrations/201608251424197_bitwise-eqipment.cs
+++ Heddoko/DAL/Migrations/201608251424197_bitwise-eqipment.cs
@@ -8,8 +8,14 @@ namespace DAL.Migrations
         public override void Up()
         {
             AlterColumn("dbo.Brainpacks", "QAStatus", c => c.Long(nullable: false));
+
+            Sql("ALTER TABLE dbo.Databoards DROP CONSTRAINT DF__Databoard__QASta__2077C861");
             AlterColumn("dbo.Databoards", "QAStatus", c => c.Long(nullable: false));
+
+            Sql("ALTER TABLE dbo.Powerboards DROP CONSTRAINT DF__Powerboar__QASta__216BEC9A");
             AlterColumn("dbo.Powerboards", "QAStatus", c => c.Long(nullable: false));
+
+
             AlterColumn("dbo.Pants", "QAStatus", c => c.Long(nullable: false));
             AlterColumn("dbo.PantsOctopis", "QAStatus", c => c.Long(nullable: false));
             AlterColumn("dbo.Sensors", "QAStatus", c => c.Long(nullable: false));
