8ebd70c130dc28275efc9a5861ab332a84f323f4
knopa <sergey@slepokurov.com>
2016-10-07 12:21:57 +0300

    added import console

---
+++ Heddoko/ImportConsole/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportConsole
+{
+    static class Utils
+    {
+        public static decimal? ToNullableDecimal(this string s)
+        {
+            decimal i;
+            if (Decimal.TryParse(s, out i))
+            {
+                return i;
+            }
+            return null;
+        }
+
+        public static int? ToNullableInt(this string s)
+        {
+            int i;
+            if (Int32.TryParse(s, out i))
+            {
+                return i;
+            }
+            return null;
+        }
+    }
+}
