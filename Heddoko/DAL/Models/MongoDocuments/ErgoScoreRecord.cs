6e0446ca8cf7589109dd1281da30fdb78989a211
bailsb <bailsb75@gmail.com>
2016-12-12 10:43:26 -0500

    Adding HourlyScore Model

---
+++ Heddoko/DAL/Models/MongoDocuments/ErgoScoreRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace DAL.Models.MongoDocuments
+{
+    public class ErgoScoreRecord
+    {
+        public ObjectId Id { get; set; } //Synced to the ID is the corresponding record 
+        public int UserID { get; set; }
+        public DateTime StartTime { get; set; }
+        public int NumFrames { get; set; }
+        public int RecordScore { get; set; }
+        public HourlyScore[] HourlyScores { get; set; }
+    }
+}
