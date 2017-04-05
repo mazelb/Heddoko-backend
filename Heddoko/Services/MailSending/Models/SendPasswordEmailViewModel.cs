ca2520cafde3c06b2be9cd46c8fc31ae5ca665e1
bailsb <bailsb75@gmail.com>
2017-04-05 12:02:22 -0400

    fixing password email

--- Heddoko/Services/MailSending/Models/SendPasswordEmailViewModel.cs
+++ Heddoko/Services/MailSending/Models/SendPasswordEmailViewModel.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿/**
+ * @file SendPasswordEmailViewModel.cs
+ * @brief Functionalities required to operate it.
+ * @author Ben Bailey (ben@heddoko.com)
+ * @date 4 2017
+ * Copyright Heddoko(TM) 2017,  all rights reserved
+*/
 namespace Services.MailSending.Models
 {
     public class SendPasswordEmailViewModel : EmailViewModel
